0d4a6a410af78cdc182796014a5657940493ac6e
Kunal Saxena <kunal.saxena@hp.com>
2016-08-04 11:27:28 +0530

    Update installStackato.sh

--- installStackato.sh
+++ installStackato.sh
@@ -104,6 +104,7 @@ bootstrap install ~/bootstrap.properties &
 
 installHSM(){
 cd ~
+mkdir LOGs
 gunzip $HCPCLIName
 echo $HCPCLIName | cut -d"."  -f 1,2,3,4 > ~/LOGs/fileHSM_2
 fileHCP_tar=`cat ~/LOGs/fileHSM_2`
