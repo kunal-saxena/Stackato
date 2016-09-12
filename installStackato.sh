05f391f210c018ce4d04011d84b494a0e1e4fccd
Kunal Saxena <kunal.saxena@hp.com>
2016-09-12 10:33:19 +0530

    Update installStackato.sh

--- installStackato.sh
+++ installStackato.sh
@@ -159,19 +159,9 @@ echo "HSM url: $hsm_url  "
 echo "Waiting for 10 sec before attaching end-point"
 sleep 10
 
-echo "echo \"Command: hsm api $hsm_url\"  " >> ~/setupFile
-echo "hsm api $hsm_url" >> ~/setupFile
-hsm api $hsm_url
-
-echo " " >> ~/setupFile
-echo "sed 's/skip-ssl-validation\": false/skip-ssl-validation\": true/g' .hsm/config.json > hsm_config.json "  >> ~/setupFile
-echo "cp hsm_config.json .hsm/config.json "  >> ~/setupFile
-echo "mv hsm_config.json ~/LOGs/ "  >> ~/setupFile
-echo " " >> ~/setupFile
-
-sed 's/"skip-ssl-validation": false/"skip-ssl-validation": true/g' .hsm/config.json > hsm_config.json
-cp hsm_config.json .hsm/config.json
-mv hsm_config.json ~/LOGs/
+echo "echo \"Command: hsm api --skip-ssl-validation $hsm_url\"  " >> ~/setupFile
+echo "hsm api --skip-ssl-validation $hsm_url" >> ~/setupFile
+hsm api --skip-ssl-validation $hsm_url
 
 echo "echo \"Command: hsm login -u admin -p '$hcp_login' " >> ~/setupFile
 echo "hsm login -u admin -p '$hcp_login' " >> ~/setupFile
